e9b8e018489f7ebfdfe982110f31809a840d24aa
mdragonborn <lena.markovic996@gmail.com>
2017-04-16 00:01:10 +0200

    Implementacija Subtitles pt1

--- Subtitles.cpp
+++ Subtitles.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "Subtitles.h"
+
+SubtitleIter Subtitles::findClosestTime(mvTime targetTime){
+    SubtitleIter begin=SubLines.begin();
+    SubtitleIter end=SubLines.end();
+    SubtitleIter mid=SubLines.begin()+(end-begin)/2;
+    if (targetTime>(*end).getTime().getEnd()) throw TimeOutOfRange();
+    while(begin!=end){
+        if((*mid).getTime().contains(targetTime)) break;
+        if((*mid).getTime().getEnd()<targetTime)
+            begin=mid+1;
+        else
+            end=mid-1;
+        mid=begin+(end-begin)/2;
+    }
+    return mid;
+};
+
+Subtitle& Subtitles::getSubtitleAtTime(mvTime time){
+    return *findClosestTime(time);
+};
+
+SubtitleIter Subtitles::begin(){
+    return SubLines.begin();
+}
+
+SubtitleIter Subtitles::end(){
+    return SubLines.end();
+}
+
+void Subtitles::pushBackNew(Subtitle subt){
+    SubLines.push_back(subt);
+};
+
+void Subtitles::insertBefore(SubtitleIter iter, Subtitle sub){
+    if ((*iter).getTime().checkOverlap(sub.getTime()) || (iter!=SubLines.begin() && (*(iter-1)).getTime().checkOverlap(sub.getTime())))
+        throw OverlappingTimeRange();
+    else SubLines.insert(iter,sub);
+};
